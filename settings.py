8f75759240f9b22f8ecf3d76ebf34cd474140ce4
inflationaryepoch <86965500+inflationaryepoch@users.noreply.github.com>
2022-01-17 19:29:23 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -17,6 +17,6 @@ if TELEGRAM_SUPPORT_CHAT_ID is None or not str(TELEGRAM_SUPPORT_CHAT_ID).lstrip(
 TELEGRAM_SUPPORT_CHAT_ID = int(TELEGRAM_SUPPORT_CHAT_ID)
 
 
-WELCOME_MESSAGE = os.getenv("WELCOME_MESSAGE", "deneme")
+WELCOME_MESSAGE = os.getenv("WELCOME_MESSAGE", "İşler iletişime hoş geldin {mention} , buraya bize iletmek istediklerinizi yazabilirsiniz. En kısa sürede dönüş yapılacaktır.")
 REPLY_TO_THIS_MESSAGE = os.getenv("REPLY_TO_THIS_MESSAGE", "REPLY_TO_THIS")
 WRONG_REPLY = os.getenv("WRONG_REPLY", "WRONG_REPLY")
